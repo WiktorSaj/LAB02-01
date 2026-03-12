c27ef288a68e2354a73d5d5556b2c40dfabba7e1
Wiktor Sajnóg <wiktorsajnog@gmail.com>
2026-03-12 11:19:57 +0100

    Method add added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+public class Math
+{
+
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+    
+}
