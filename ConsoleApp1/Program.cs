aa77320eb2a349a0663ea64b8450b07b98eca16a
Wiktor Sajnóg <wiktorsajnog@gmail.com>
2026-03-18 18:41:48 +0100

    Renamed variables in add method to first and second

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,9 +5,9 @@ Console.WriteLine("Hello, World!");
 public class Math
 {
 
-    static int add(int a, int b)
+    static int add(int first, int second)
     {
-        return a + b;
+        return first + second;
     }
 
     static int sub(int a, int b)
